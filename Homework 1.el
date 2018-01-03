55bfd9a0c288c2638b278331f7f1911dfb05fdc1
Joseph Lyons <JosephTLyons@users.noreply.github.com>
2018-01-03 07:42:18 -0500

    Comment

--- Homework 1.el
+++ Homework 1.el
@@ -1,6 +1,7 @@
 ;; Joseph Lyons
 ;; C311
 ;; Homework 1
+;; Expressions are followed by their evaluated forms in the next line
 
 (* (log 15.0 10) (+ 3 (* 18 2))) ; Assumed log used base 10
 45.86755910317157
