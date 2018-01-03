26017db9113c9b6367c99ea5e3ca7b40bdcb2182
Joseph Lyons <JosephTLyons@users.noreply.github.com>
2018-01-03 07:21:01 -0500

    A few problems complete

--- Homework 1.el
+++ Homework 1.el
@@ -4,6 +4,6 @@
 ; log(15.0) * (3 + 18 * 2)
 (max 11 37 21 15 8)
 (>= (* 3 3.14) (/ 15 2))
-; a symbolic list containing 5 names of trees.
-; the first element of the list above by a function call
+'(Maple Pine Hickory Elm Fir)
+(car '(Maple Pine Hickory Elm Fir))
 ;a function call resulting in a string telling us how many minutes are in a day. The minutes should be computed as a multiplication directly in the expression.
