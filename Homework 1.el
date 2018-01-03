dc6e3eec92736761a5b4866d5be6c328fe44e9f5
Joseph Lyons <JosephTLyons@users.noreply.github.com>
2018-01-03 07:40:42 -0500

    Evaluated problems done

--- Homework 1.el
+++ Homework 1.el
@@ -3,8 +3,18 @@
 ;; Homework 1
 
 (* (log 15.0 10) (+ 3 (* 18 2))) ; Assumed log used base 10
+45.86755910317157
+
 (max 11 37 21 15 8)
+37
+
 (>= (* 3 3.14) (/ 15 2))
+t
+
 '(Maple Pine Hickory Elm Fir)
+(Maple Pine Hickory Elm Fir)
+
 (car '(Maple Pine Hickory Elm Fir))
+Maple
+
 ;a function call resulting in a string telling us how many minutes are in a day. The minutes should be computed as a multiplication directly in the expression.
