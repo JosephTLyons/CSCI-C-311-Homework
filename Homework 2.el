96af3ee3434f73c6e49d05cd4c245b5a9df586bb
Joseph Lyons <JosephTLyons@users.noreply.github.com>
2018-01-05 03:39:54 -0500

    Began work on homework 2

--- Homework 2.el
+++ Homework 2.el
@@ -2,3 +2,9 @@
 ;; C311
 ;; Homework 2
 ;; Expressions are followed by their evaluated forms in the next line
+
+(setq age 27)
+(setq zipCode 46563)
+(setq year 2018) ; Add description for one of 3 above
+
+
