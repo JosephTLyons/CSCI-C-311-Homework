6e1f07c01e4f5493ab63f183b7de9834a529ec76
Joseph Lyons <JosephTLyons@users.noreply.github.com>
2018-01-05 03:45:56 -0500

    Defined, then assigned, and added a description to one

--- Homework 2.el
+++ Homework 2.el
@@ -3,8 +3,12 @@
 ;; Homework 2
 ;; Expressions are followed by their evaluated forms in the next line
 
+(defvar age)
+(defvar zipCode "I live in Plymouth Indiana")
+(defvar year)
+
 (setq age 27)
 (setq zipCode 46563)
-(setq year 2018) ; Add description for one of 3 above
+(setq year 2018)
 
 
