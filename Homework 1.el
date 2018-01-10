9de21cc95b6fa666e3265d8b6133dde0a5e015d7
Joseph Lyons <JosephTLyons@users.noreply.github.com>
2018-01-10 13:21:19 -0500

    Fixed log with base 2

--- Homework 1.el
+++ Homework 1.el
@@ -3,8 +3,8 @@
 ;; Homework 1
 ;; Expressions are followed by their evaluated forms in the next line
 
-(* (log 15.0 10) (+ 3 (* 18 2))) ; Assumed log used base 10
-45.86755910317157
+(* (log 15.0 2) (+ 3 (* 18 2))) ; Assumed log used base 2
+152.36873322873222
 
 (max 11 37 21 15 8)
 37
