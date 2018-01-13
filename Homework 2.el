52de39fce227c8888b0fe443bfa37cf15daf9cdc
Joseph Lyons <JosephTLyons@users.noreply.github.com>
2018-01-13 14:50:56 -0500

    More work on homework 2

--- Homework 2.el
+++ Homework 2.el
@@ -3,14 +3,27 @@
 ;; Homework 2
 ;; Expressions are followed by their evaluated forms in the next line
 
+;; Declare 3 variables
 (defvar age)
+age
+
 (defvar savings "Money in savings account")
+savings
+
 (defvar year)
+year
 
+;; Assign values to these variables
 (setq age 27)
-(setq savings -30)
-(setq year 2018)
+27
 
+(setq savings -30)
+-30
+
+(setq year 2018)
+2018
+
+;; Use these variables in expressions (expt, random, abs, cos, truncate)
 (expt age 2)
 729
 
@@ -21,7 +34,8 @@
 30
 
 (cos age)
+-0.2921388087338362
 
 (truncate (* 3.14 year))
-
+6336
 
