73a0f5eca5e581149cfcc0c1877f1f1b8c0ee44e
Joseph Lyons <JosephTLyons@users.noreply.github.com>
2018-01-15 17:08:18 -0500

    Added a C++ folder
    For more difficult problems, I will write the solution first in C++, then convert them to Elisp.  This folder contains my C++ solutions.

---
+++ C++ Solutions/Recursive Product/Recursive Product/main.cpp
@@ -0,0 +1,38 @@
+// This file was used to help make a solution for the recursive prod-list method needed in homework 2
+
+#include <iostream>
+#include <stack>
+
+int product (std::stack<int> &listOfNumbers)
+{
+    if (listOfNumbers.empty())
+        return 0;
+    
+    else
+    {
+        if (listOfNumbers.size() == 1)
+            return listOfNumbers.top();
+        
+        int topNumber = listOfNumbers.top();
+        
+        listOfNumbers.pop();
+        
+        return (topNumber * product (listOfNumbers));
+    }
+}
+
+int main()
+{
+    std::stack<int> listOfNumbers;
+    
+    listOfNumbers.push (1);
+    listOfNumbers.push (2);
+    listOfNumbers.push (3);
+    listOfNumbers.push (4);
+    listOfNumbers.push (5);
+    listOfNumbers.push (6);
+    
+    std::cout << product (listOfNumbers) << std::endl;
+    
+    return 0;
+}
