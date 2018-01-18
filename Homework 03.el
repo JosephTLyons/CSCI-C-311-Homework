227221957a3021ba693a964136e1153fcc4951f7
Joseph Lyons <JosephTLyons@users.noreply.github.com>
2018-01-18 02:37:11 -0500

    Formatting of the nested statements

--- Homework 03.el
+++ Homework 03.el
@@ -4,7 +4,8 @@
 ;; Expressions are followed by their evaluated forms in the next line
 
 (defun is-const (L)
-  (if (not L) t
+  (if (not L) 
+      t
     (if (not (cdr L))
 	t
       ()))) ; add loop here
