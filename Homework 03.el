97c28811baae129dc15ca23296e369a2e8546d00
Joseph Lyons <JosephTLyons@users.noreply.github.com>
2018-01-18 02:36:35 -0500

    Working on first function of Homework 03.el

--- Homework 03.el
+++ Homework 03.el
@@ -2,3 +2,13 @@
 ;; C311 - Spring 2018
 ;; Homework 3
 ;; Expressions are followed by their evaluated forms in the next line
+
+(defun is-const (L)
+  (if (not L) t
+    (if (not (cdr L))
+	t
+      ()))) ; add loop here
+
+(is-const '())
+(is-const '(1))
+
