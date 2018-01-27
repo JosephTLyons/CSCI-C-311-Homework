e6fd54222809963f2f5c3a6b6901716653258cd1
Joseph Lyons <JosephTLyons@users.noreply.github.com>
2018-01-27 04:00:30 -0500

    Started working on Homework 04.el

--- Homework 04.el
+++ Homework 04.el
@@ -2,3 +2,17 @@
 ;; C311 - Spring 2018
 ;; Homework 4
 ;; Expressions are followed by their evaluated forms in the next line
+
+;; Part 1 - Not Done
+
+;; Part 2 - Not Done
+;; Logic: divisor-counter = 2 since all positive integers are divisible
+;; by one and themselves
+;; halfway-mark = (number / 2) because we dont need to waste time checking
+;; values past the halfway mark, since they won't be evenly divide the
+;; number were checking   
+(defun is-prime (number)
+  (let ((prime t) (divisor-counter 2) (halfway-mark (/ number 2)))
+    (dotimes (2 halfway-mark prime)
+      
+;; Part 3 - Not Done
