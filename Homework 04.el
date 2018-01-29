d121ac9629362cced0a42b302f1572783ac186d1
Joseph Lyons <JosephTLyons@users.noreply.github.com>
2018-01-29 03:04:34 -0500

    Testing some examples

--- Homework 04.el
+++ Homework 04.el
@@ -22,3 +22,36 @@
     (if (equal divisor-count 2)
         t
       nil)))
+
+(is-prime 0)
+;nil
+
+(is-prime 1)
+;nil
+
+(is-prime 2)
+;t
+
+(is-prime 3)
+;t
+
+(is-prime 4)
+t
+
+(is-prime 5)
+;t
+
+(is-prime 6)
+t
+
+(is-prime 7)
+;t
+
+(is-prime 8)
+;nil
+
+(is-prime 9)
+t
+
+
+;; Part 3 - Not Done
