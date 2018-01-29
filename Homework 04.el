31d0bd8d7e44ede4803a2066492160fe346cedf0
Joseph Lyons <JosephTLyons@users.noreply.github.com>
2018-01-29 03:23:16 -0500

    Working solution with tests corrected
    This is a very close solution to the C++ version

--- Homework 04.el
+++ Homework 04.el
@@ -5,11 +5,17 @@
 
 ;; Part 1 - Not Done
 
-;; Part 2 - Not Done
+;; Part 2
+;; Logic: When a number has more than 2 divisors, it is not prime.
+;; We execute the body of the loop while divisor (starting at 1) < quotient,
+;; which is equivalent to x (starting at 0) < (quotient - 1), since we cant set
+;; x to 1 in dotimes loops.
+;; Each loop, increment the divisor number and recalculate the quotient
+;; to cut down the amount of loop executions needed - testing from 1 to 
+;; The number checked is wasteful.
 (defun is-prime (number)
-  (let ((divisor-count 0) (quotient number))
-    (dotimes (divisor quotient)
-      (setq divisor (+ 1 divisor))
+  (let ((divisor 1) (divisor-count 0) (quotient number))
+    (dotimes (x (- quotient 1))
       (setq quotient (/ number divisor))
       (if (equal (mod number divisor) 0)
           (setq divisor-count (+ 2 divisor-count)))
@@ -31,13 +37,13 @@
 ;t
 
 (is-prime 4)
-t
+;nil
 
 (is-prime 5)
 ;t
 
 (is-prime 6)
-t
+;nil
 
 (is-prime 7)
 ;t
@@ -46,7 +52,7 @@ t
 ;nil
 
 (is-prime 9)
-t
+;nil
 
 
 ;; Part 3 - Not Done
