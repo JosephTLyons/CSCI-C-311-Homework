8bf44648cf0c2cfe0317d819a76e656160f484e6
Joseph Lyons <JosephTLyons@users.noreply.github.com>
2018-01-31 20:24:27 -0500

    Added GCD C++ version

---
+++ C++ Solutions/Homework 04 GCD.cpp
@@ -0,0 +1,30 @@
+//
+//  main.cpp
+//  Homework
+//
+//  Created by Joseph Lyons on 1/31/18.
+//  Copyright © 2018 Joseph Lyons. All rights reserved.
+//
+
+#include <iostream>
+
+int GCD (const int &n, const int &m)
+{
+    int remainder = 1;
+    int dividend = n;
+    int divisor = m;
+    
+    while (remainder != 0)
+    {
+        remainder = dividend % divisor;
+        dividend = divisor;
+        divisor = remainder;
+    }
+    
+    return dividend;
+}
+
+int main()
+{
+    std::cout << GCD (500, 24);
+}
