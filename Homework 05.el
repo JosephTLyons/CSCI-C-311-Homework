0011d6db129decc1ce47e42d2e8fb36dfc3c832c
Joseph Lyons <JosephTLyons@users.noreply.github.com>
2018-02-07 01:41:43 -0500

    Did part 2a

--- Homework 05.el
+++ Homework 05.el
@@ -2,3 +2,34 @@
 ;; C311 - Spring 2018
 ;; Homework 5
 ;; Expressions are followed by their evaluated forms in the next line
+
+
+;; Part 1a
+
+;; Part 1b
+
+;; Part 2a
+(defun make-multiples (n m)
+  "Returns a list of multiples of value n, from 1 to m"
+  (let ((temp-list '()))
+    (dotimes (x m)
+      (setq temp-list (append temp-list (list (* (+ x 1) n)))))
+    temp-list))
+
+(make-multiples 1 1) ; Testing some simple cases
+;(1)
+
+(make-multiples 1 6)
+;(1 2 3 4 5 6)
+
+(make-multiples 2 3) ; Testing more interesting values
+;(2 4 6)
+
+(make-multiples 3 8)
+;(3 6 9 12 15 18 21 24)
+
+(make-multiples 4 12)
+;(4 8 12 16 20 24 28 32 36 40 44 48)
+
+
+;; Part 2b
