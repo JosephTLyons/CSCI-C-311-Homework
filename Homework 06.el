407fbafe4deba927d028d9f3966b70c5d49a8b61
Joseph Lyons <JosephTLyons@users.noreply.github.com>
2018-02-15 03:35:51 -0500

    Formatting

--- Homework 06.el
+++ Homework 06.el
@@ -121,11 +121,11 @@
         (setq counter (+ counter 1))))))
 
 (calendar 4 28)      ; February 2018
-;S  M  T  W  T  F  S  
-;            1  2  3  
-;4  5  6  7  8  9  10 
-;11 12 13 14 15 16 17 
-;18 19 20 21 22 23 24 
+;S  M  T  W  T  F  S
+;            1  2  3
+;4  5  6  7  8  9  10
+;11 12 13 14 15 16 17
+;18 19 20 21 22 23 24
 ;25 26 27 28 nil
 
 (calendar 0 30)      ; Testing all cases 0 - 6 with different day amounts
