68435ff05b7828ca35481f4a70193d31007bc5f4
Joseph Lyons <JosephTLyons@users.noreply.github.com>
2018-02-14 23:17:30 -0500

    Part 2 of Homework06.el done

--- Homework 06.el
+++ Homework 06.el
@@ -23,8 +23,48 @@
 ;;       |           |         |           |             |           |         |           |
 ;;   m(| 17 |)   m(| 8 |)   m(| 3 |)   m(| 11 |)     m(| 18 |)   m(| 2 |)  m(| 6 |)    m(| 1 |)
 
+
 ;; Part 2a
 ;; Symbols in Lisp
+(defun next-day (day)
+  (let ((next))
+    (if (equal "sunday" (symbol-name day))
+        (setq next 'monday)
+      (if (equal "monday" (symbol-name day))
+          (setq next 'tuesday)
+        (if (equal "tuesday" (symbol-name day))
+            (setq next 'wednesday)
+          (if (equal "wednesday" (symbol-name day))
+              (setq next 'thursday)
+            (if (equal "thursday" (symbol-name day))
+                (setq next 'friday)
+              (if (equal "friday" (symbol-name day))
+                  (setq next 'saturday)
+                (if (equal "saturday" (symbol-name day))
+                    (setq next 'sunday))))))))
+    next))
+
+(next-day 'sunday)
+;monday
+
+(next-day 'monday)
+;tuesday
+
+(next-day 'tuesday)
+;wednesday
+
+(next-day 'wednesday)
+;thursday
+
+(next-day 'thursday)
+;friday
+
+(next-day 'friday)
+;saturday
+
+(next-day 'saturday)
+;sunday
+
 
 ;; Part 2b
 ;; Symbols in Lisp
