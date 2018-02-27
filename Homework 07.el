b2bc682ae16a890553ad85259c47200ace0d8c4a
Joseph Lyons <JosephTLyons@users.noreply.github.com>
2018-02-27 05:37:48 -0500

    Testing when it is not an array

--- Homework 07.el
+++ Homework 07.el
@@ -34,6 +34,9 @@
 (bubble-sort '[2000 50 12 70000 400 300 1]) ; Testing bigger numbers
 ;[1 12 50 300 400 2000 70000]
 
+(bubble-sort 'a) ; Simply returns item if it is not an array
+;a
+
 
 ;; Part 2
 ;; Transform the following recursive function into an iterative one:
