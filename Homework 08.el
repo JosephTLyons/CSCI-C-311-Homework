37c14426c9e02a2f54de05e7eece48faa5119692
Joseph Lyons <JosephTLyons@users.noreply.github.com>
2018-02-28 20:22:06 -0500

    Part 1 of Homework 08 done

--- Homework 08.el
+++ Homework 08.el
@@ -2,3 +2,18 @@
 ;; C311 - Spring 2018
 ;; Homework 8
 ;; Expressions are followed by their evaluated forms in the next line
+
+;; Part 1
+(defun bsymbol (&rest L)
+    (dolist (x L)
+      (if (symbolp x)
+          (if (equal (substring (symbol-name x) 0 1) "b")
+              (progn
+                (princ (symbol-name x))
+                (princ " ")))))
+    (princ "\n")
+    t)
+
+(bsymbol 'boot 3 'basic 'balloon 8 'march)
+;boot basic balloon 
+;t
