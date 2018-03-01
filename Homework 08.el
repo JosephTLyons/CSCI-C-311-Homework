525296c529d8a2f6fabb4fd02998bd59c2bc80e9
Joseph Lyons <JosephTLyons@users.noreply.github.com>
2018-02-28 20:27:42 -0500

    Added some extra test cases

--- Homework 08.el
+++ Homework 08.el
@@ -14,6 +14,14 @@
     (princ "\n")
     t)
 
-(bsymbol 'boot 3 'basic 'balloon 8 'march)
-;boot basic balloon 
+(bsymbol 'boot 3 'basic 'balloon 8 'march) ; Test given in problem
+;boot basic balloon
+;t
+
+(bsymbol 'bat 'girl 'boy 'a 'b 83 "man") ; Test with mixture of elements
+;bat boy b
+;t
+
+(bsymbol 'superman 'batman 'bane 'black-panter 'wonder-woman 'spider-man 'bruce-banner) ; Long test
+;batman bane black-panter bruce-banner
 ;t
