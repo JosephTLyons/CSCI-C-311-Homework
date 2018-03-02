a0bce3213a6c75085600b2f5cd4da18aa1534970
Joseph Lyons <JosephTLyons@users.noreply.github.com>
2018-03-02 02:19:00 -0500

    Adding proper-power

---
+++ Miscellaneous/Proper Power.el
@@ -0,0 +1,24 @@
+;; proper-power can compute both positive and negative exponent problems.
+;; However, the algorithm doesn't work properly for exponents that are floats.
+
+(defun proper-power (base exponent)
+  "Computes (base)^(exponent)."
+  (let ((result 1))
+    (cond
+     ((= exponent 0) 1)
+     ((> exponent 0)
+      (while (> exponent 0)
+        (setq exponent (- exponent 1))
+        (setq result (* result base))))
+     ((< exponent 0)
+      (while (< exponent 0)
+        (setq exponent (+ exponent 1))
+        (setq result (* result (/ 1 (* base 1.0)))))))
+    result))
+
+(proper-power 3 3)
+;27
+
+(proper-power 3 -3)
+;0.037037037037037035
+
