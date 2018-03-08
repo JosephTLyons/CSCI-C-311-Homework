534ad820537be10b6cc92f548b44fca078bd220e
Joseph Lyons <JosephTLyons@users.noreply.github.com>
2018-03-07 23:04:11 -0500

    Removed problems that are not on the homework now

--- Homework 08.el
+++ Homework 08.el
@@ -3,25 +3,14 @@
 ;; Homework 8
 ;; Expressions are followed by their evaluated forms in the next line
 
-;; PART 1A
-(defun bsymbol (&rest L)
-    (dolist (x L)
-      (if (symbolp x)
-          (if (equal (substring (symbol-name x) 0 1) "b")
-              (progn
-                (princ (symbol-name x))
-                (princ " ")))))
-    (princ "\n")
-    t)
+;; PART 1
+;; Dynamic Programming
 
-(bsymbol 'boot 3 'basic 'balloon 8 'march) ; Test given in problem
-;boot basic balloon
-;t
+;; PART 2
+;; Dynamic Programming
 
-(bsymbol 'bat 'girl 'boy 'a 'b 83 "man") ; Test with mixture of elements
-;bat boy b
-;t
+;; PART 3
+;; Deep Recursion
 
-(bsymbol 'superman 'batman 'bane 'black-panter 'wonder-woman 'spider-man 'bruce-banner) ; Long test
-;batman bane black-panter bruce-banner
-;t
+;; PART 4
+;; General Transformation
