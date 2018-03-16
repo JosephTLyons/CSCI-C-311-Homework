9d19e6ddc7f91b20b1d2090c7bd9b09825f112ae
Joseph Lyons <JosephTLyons@users.noreply.github.com>
2018-03-16 01:17:13 -0400

    Done with version that works with real numbers

--- Miscellaneous/Quadratic.el
+++ Miscellaneous/Quadratic.el
@@ -2,18 +2,16 @@
   "A modified sqrt function to deal with imaginary numbers."
   (let ((result '()))
     (if (> x 0)
-        (sqrt x)
-      (push "imaginary" result))
-      (push (sqrt (* x -1)) result)
+        (setq result (sqrt x))
+      (push "imaginary" result)
+      (push (sqrt (* x -1)) result))
     result))
 
 (defun quadratic (a b c)
   "Calculates the zeroes of a quadratic function by inputing the coefficients."
-  (let (results '())
-    (push (/ (+ (* b -1) (sqrt2 (- (expt b 2) (* 4 a c)))) (* 2 a)) results)
-    (push (/ (- (* b -1) (sqrt2 (- (expt b 2) (* 4 a c)))) (* 2 a)) results)
+  (let ((results '()))
+    (push (/ (+ (* b -1) (sqrt (- (expt b 2) (* 4 a c)))) (* 2 a)) results)
+    (push (/ (- (* b -1) (sqrt (- (expt b 2) (* 4 a c)))) (* 2 a)) results)
     results))
 
-(quadratic 1 0 9)
-6.0 imaginary
-
+(quadratic 1 0 -9)
