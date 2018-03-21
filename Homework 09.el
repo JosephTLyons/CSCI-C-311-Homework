6395e23a598111065459bb5534c6144f4e6272f1
Joseph Lyons <JosephTLyons@users.noreply.github.com>
2018-03-21 19:36:21 -0400

    Part 1 done

--- Homework 09.el
+++ Homework 09.el
@@ -2,3 +2,24 @@
 ;; C311 - Spring 2018
 ;; Homework 9
 ;; Expressions are followed by their evaluated forms on the next line
+
+
+;; PART 1
+;; Lambda Expression
+(defun replace (L z y)
+  "Replaces all occurances of z in a list with y."
+  (let ((lam (lambda (x) (if (equal x z) y x))))
+    (mapcar lam L)))
+
+(replace '(3 1 5 6 3 2 3) 3 9)
+;(9 1 5 6 9 2 9)
+
+(replace '("dog" "cat" "man") "man" "mouse")
+;("dog" "cat" "mouse")
+
+(replace '(9 a x 3.14 "house" x) 'x 'j)
+;(9 a j 3.14 "house" j)
+
+
+;; PART 2
+;; Formal Grammar
